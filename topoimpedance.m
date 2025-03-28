411f675d3f98d1d7c46e7d6848f1b3cb7019d3a5
Arnaud Delorme <arnodelorme@gmail.com>
2025-03-28 16:18:54 -0700

    add update

--- topoimpedance.m
+++ topoimpedance.m
@@ -2,6 +2,7 @@ function topoimpedance(Values, loc_file, varargin)
 % topoimpedance() - plot electrode locations with impedance circles
 % Usage:
 %        >>  topoimpedance(impedance_values, EEG.chanlocs);
+%        >>  topoimpedance(impedance_values, EEG.chanlocs, 'update', figure_handle);
 % Inputs:
 %   Values        - vector of impedance values for each electrode
 %   loc_file      - EEG.chanlocs structure or location file
@@ -11,14 +12,12 @@ function topoimpedance(Values, loc_file, varargin)
 %   'plotrad'     - [0.15<=float<=1.0] plotting radius {default: 0.5}
 %   'nosedir'     - ['+X'|'-X'|'+Y'|'-Y'] direction of nose {default: '+X'}
 %   'verbose'     - ['on'|'off'] {default: 'off'}
+%   'update'      - figure handle for updating existing plot
 
 % Set defaults
-VERBOSE = 'off';
 ELECTRODES = 'on';
 HEADCOLOR = [0 0 0];
-BACKCOLOR = [.93 .96 1];
 rmax = 0.5;             % actual head radius
-plotrad = 0.5;          % plotting radius
 headrad = 0.5;          % head radius
 CIRCGRID = 201;         % number of angles to use in drawing circles
 AXHEADFAC = 1.3;        % head to axes scaling factor
@@ -26,9 +25,13 @@ HLINEWIDTH = 2;         % default linewidth for head, nose, ears
 DISKSIZE = 0.04;        % constant size for impedance disks
 DISKBORDER = 1;         % border width for disks
 THRESHOLD = 0.5;        % threshold for red/green coloring
+EFSIZE = get(0,'DefaultAxesFontSize'); % use current default fontsize for electrode labels
+figHandle = [];
 
-% Parse optional arguments
-if nargin > 2
+% Parse remaining optional arguments
+
+% Check if this is an update call
+if ~isempty(varargin)
     for i = 1:2:length(varargin)
         Param = varargin{i};
         Value = varargin{i+1};
@@ -37,25 +40,39 @@ if nargin > 2
         end
         Param = lower(Param);
         switch Param
-            case 'verbose'
-                VERBOSE = Value;
             case 'electrodes'
                 ELECTRODES = lower(Value);
             case 'headrad'
                 headrad = Value;
-            case 'plotrad'
-                plotrad = Value;
-            case 'nosedir'
-                NOSEDIR = Value;
+            case 'update'
+                figHandle = Value;
         end
     end
 end
 
+% If this is an update call, just update the disk colors
+if ~isempty(figHandle)
+    if ~ishandle(figHandle)
+        error('Invalid figure handle provided for update');
+    end
+    figure(figHandle);
+    ax = gca;
+    patches = findobj(ax, 'Type', 'patch');
+    
+    % Update colors based on new values
+    for i = 1:length(patches)
+        if Values(i) > THRESHOLD
+            set(patches(i), 'FaceColor', [1 0 0]);  % red for high impedance
+        else
+            set(patches(i), 'FaceColor', [0 1 0]);  % green for low impedance
+        end
+    end
+    return;
+end
+
 % Read channel locations
-if ischar(loc_file)
-    [tmpeloc labels Th Rd indices] = readlocs(loc_file);
-elseif isstruct(loc_file)
-    [tmpeloc labels Th Rd indices] = readlocs(loc_file);
+if ischar(loc_file) || isstruct(loc_file)
+    [~, labels, Th, Rd] = readlocs(loc_file);
 else
     error('loc_file must be a EEG.locs struct or locs filename');
 end
@@ -72,7 +89,8 @@ y = real(allcoords);
 % Set up the plot
 cla
 hold on
-set(gca,'Xlim',[-rmax rmax]*AXHEADFAC,'Ylim',[-rmax rmax]*AXHEADFAC);
+% Add DISKSIZE to the limits to show full disks
+set(gca,'Xlim',[-rmax-DISKSIZE rmax+DISKSIZE]*AXHEADFAC,'Ylim',[-rmax-DISKSIZE rmax+DISKSIZE]*AXHEADFAC);
 axis square
 axis off
 
@@ -118,21 +136,19 @@ if ~isempty(Values)
         % Draw filled circle with border
         patch(circle_x, circle_y, diskcolor, 'EdgeColor', HEADCOLOR, ...
               'LineWidth', DISKBORDER);
-    end
-end
-
-% Add electrode labels if requested
-if strcmp(ELECTRODES,'labels')
-    for i = 1:length(labels)
-        text(x(i),y(i),labels(i,:),'HorizontalAlignment','center',...
-             'VerticalAlignment','middle','Color',HEADCOLOR);
-    end
-elseif strcmp(ELECTRODES,'numbers')
-    for i = 1:length(labels)
-        text(x(i),y(i),int2str(i),'HorizontalAlignment','center',...
-             'VerticalAlignment','middle','Color',HEADCOLOR);
+        
+        % Add electrode label inside disk
+        if strcmp(ELECTRODES,'labels')
+            text(x(i),y(i),labels(i,:),'HorizontalAlignment','center',...
+                 'VerticalAlignment','middle','Color',HEADCOLOR,...
+                 'FontSize',EFSIZE);
+        elseif strcmp(ELECTRODES,'numbers')
+            text(x(i),y(i),int2str(i),'HorizontalAlignment','center',...
+                 'VerticalAlignment','middle','Color',HEADCOLOR,...
+                 'FontSize',EFSIZE);
+        end
     end
 end
 
 hold off
-end 
+end
